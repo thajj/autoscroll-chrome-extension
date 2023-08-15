dcbab73569b24fe25cdb97be2523c01fb34236a2
thajj <hajj.toufic@gmail.com>
2023-08-15 09:32:08 -0400

    Init

---
+++ autoscroll.js
@@ -0,0 +1,10 @@
+/**
+  * autoscrollF
+  */
+$(document).ready(function() {
+   $('html, body').animate({
+	        scrollTop: $(window).scrollTop() + 10000
+	    }, 30000);
+	    return false;
+
+});
